21a96d3a558c6414e48a3742feb98597b76c0983
Carlos Ruiz <cardaniruiz@gmail.com>
2022-03-28 22:59:25 -0500

    Updating Jenkinsfile steps
    - Fixing Docker Login 
    - Adding SSH commands

--- vars/pipelineTimeOffCICD.groovy
+++ vars/pipelineTimeOffCICD.groovy
@@ -16,24 +16,30 @@ def call(body) {
         stages {
             stage('Docker Hub login') {
                 steps {
-                    utilDockerLogin dockerLoginUsername = $DOCKER_HUB_CREDS_USR dockerLoginPassword = $DOCKER_HUB_CREDS_PSW
+                    script {
+                        //utilDockerLogin dockerLoginUsername = "$DOCKER_HUB_CREDS_USR", dockerLoginPassword = "$DOCKER_HUB_CREDS_PSW"
+                        sh 'docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW'
+                    }
                 }
             }
-            stage('TimeOff Build - Docker Steps') {
+            stage('TimeOff Build - Docker Publish Steps') {
                 steps {
                     // Docker Build
                     utilDockerBuild taggedImage: taggedImage, dockerfileName: dockerfileName, dockerBuildContext: dockerBuildContext
                     // Docker Push
-                    utilDockerPush taggedImage: taggedImage
+                    utilDockerPush taggedImage: taggedImagee
+                }
+            }
+            stage('TimeOff Deployment - Docker Compose Update and Clean') {
+                steps {
+                    withCredentials(bindings: [sshUserPrivateKey(credentialsId: 'Vagrant', \
+                                                keyFileVariable: 'SSH_KEY_FOR_VAGRANT')]) {
+                    sh'ssh -o "StrictHostKeyChecking=no" -i $SSH_KEY_FOR_VAGRANT vagrant@192.168.100.2 "docker-compose pull && docker-compose up --build --force-recreate -d && docker image prune --force --all"'
+                    }
                     // Docker Clean
                     utilDockerRemove taggedImage: taggedImage
                 }
             }
-            // stage('TimeOff Deployment - Docker Compose Update') {
-            //     steps {
-            //         // WIP
-            //     }
-            // }
         }
     }
 }
